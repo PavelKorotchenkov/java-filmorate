f1c2b14e53313071acbee2d3467b7710ea643c71
Pavel Korotchenkov <radiel13@gmail.com>
2023-12-20 09:52:36 +0200

    quick fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationExceptionHandler.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 @Slf4j
 @ControllerAdvice
 public class ValidationExceptionHandler {
