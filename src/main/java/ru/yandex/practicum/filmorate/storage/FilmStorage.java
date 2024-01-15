c338a26ebed275387691609fbe6c5d6ab8a79ea7
Pavel Korotchenkov <radiel13@gmail.com>
2024-01-15 22:27:36 +0200

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+
 @Component
 public interface FilmStorage {
 	Film findFilm(Long filmId);
+
 	Collection<Film> getAllFilms();
+
 	Film add(Film film);
+
 	Film delete(Long filmId);
+
 	Film update(Film film);
 }
