328093e321faaea0fb3fb67c2c5e5142f7cbc87c
Pavel Korotchenkov <radiel13@gmail.com>
2024-01-15 23:03:25 +0200

    fix: tests

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -20,8 +21,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @RequiredArgsConstructor
 class UserControllerTest {
+	@Autowired
 	private MockMvc mockMvc;
+	@Autowired
 	private ObjectMapper objectMapper;
+	@Autowired
 	private UserController controller;
 
 	private User user;
