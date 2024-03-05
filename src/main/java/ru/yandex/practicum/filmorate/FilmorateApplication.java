0dee4151ef4accb11c8f67fd1d2a236ca3bfc9d0
Pavel Korotchenkov <radiel13@gmail.com>
2024-03-05 20:37:02 +0200

    Создана ветка add-reviews

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "ru.yandex.practicum.filmorate")
 public class FilmorateApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
