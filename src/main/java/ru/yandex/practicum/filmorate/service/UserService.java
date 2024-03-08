0ba20d5f0ad67ee8ca9d4c00f3b0d6d327327092
sasha1Gury <sashagury456123@mail.ru>
2024-03-08 15:09:49 +0300

    feat: add DI and getRecommendation for UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
+import ru.yandex.practicum.filmorate.storage.impl.RecommendationDbStorage;
 
 import java.util.List;
 
@@ -12,10 +14,12 @@ import java.util.List;
 @Service
 public class UserService {
 	private final UserStorage userStorage;
+	private final RecommendationDbStorage recommendationDbStorage;
 
 	@Autowired
-	public UserService(UserStorage userStorage) {
+	public UserService(UserStorage userStorage, RecommendationDbStorage recommendationDbStorage) {
 		this.userStorage = userStorage;
+		this.recommendationDbStorage = recommendationDbStorage;
 	}
 
 	public User addUser(User user) {
@@ -40,6 +44,10 @@ public class UserService {
 			log.info("Имя пользователя не заполнено, присвоено имя: {}", user.getName());
 		}
 		return userStorage.update(user);
+	}
+
+	public List<Film> getRecommendation(long id) {
+		return recommendationDbStorage.getRecommendation(id);
 	}
 
 	private boolean validateUserName(User user) {
