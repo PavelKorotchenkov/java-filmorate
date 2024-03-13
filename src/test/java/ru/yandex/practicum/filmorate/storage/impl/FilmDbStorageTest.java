b39d2570c1419288e1638f1d337fb1dbc8c50719
Pavel Korotchenkov <radiel13@gmail.com>
2024-03-13 20:01:21 +0200

    checkstyle fix

--- src/test/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/impl/FilmDbStorageTest.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
