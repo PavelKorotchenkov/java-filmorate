0496a9f015045318b71682ff43b562bc6e1e5201
Pavel Korotchenkov <radiel13@gmail.com>
2024-03-15 17:49:41 +0200

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FeedStorage.java
@@ -5,9 +5,9 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface FeedStorage {
-    Event add(Long userId, Long entityId, String operation, String eventType);
+	Event add(Long userId, Long entityId, String operation, String eventType);
 
-    List<Event> getByUserId(Long userId);
+	List<Event> getByUserId(Long userId);
 
-    void delete(Long entityId);
+	void delete(Long entityId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RecommendationStorage.java
@@ -5,5 +5,5 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface RecommendationStorage {
-    List<Film> getById(long id);
+	List<Film> getById(long id);
 }
